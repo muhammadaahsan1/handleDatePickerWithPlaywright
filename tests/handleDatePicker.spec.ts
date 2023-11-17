9c446db2a7d0179021a8aa7943b2a28910bf0697
muhammadaahsan1 <muhammad_aahsan@outlook.com>
2023-11-17 12:14:01 +0500

    datepicker17

--- tests/handleDatePicker.spec.ts
+++ tests/handleDatePicker.spec.ts
@@ -2,24 +2,54 @@ import { test, expect } from '@playwright/test';
 
 test('test DatePicker', async ({ page }) => {
   await page.goto('https://testautomationpractice.blogspot.com/');
-  const year = "2023";
-  const month = "October";
+  const targetYear = "2024";
+  const targetMonth = "October";
 
-  await page.click('#datepicker') // opens calender
+
+  await page.click('#datepicker'); // opens calender
+
   while(true)
   {
-    const currentYear = await page.locator('.ui-datepicker-year').textContent();
-    const currentMonth = await page.locator('.ui-datepicker-month').textContent();
-    if (currentYear== year && currentMonth==month)
-    {
-      break;
+    const currentYear = await page.locator('.ui-datepicker-year').first().textContent();
+    
+    const currentMonth = await page.locator('.ui-datepicker-month').first().textContent();
+    function getMonthNumberFromName(monthName) {
+      return new Date(`${monthName} 1, 2022`).getMonth() + 1;
     }
-    if(currentYear<year){
-      await page.locator('[title="Next"]')
+
+    const currmonthNumber = getMonthNumberFromName(currentMonth) 
+    const targetMonthInNumber = getMonthNumberFromName(targetMonth) 
+    
+   
+
+    if(currentYear== targetYear && currmonthNumber==targetMonthInNumber)
+      {
+        break;
+      }
+      else if(currentYear<targetYear){
+          await page.locator('[title="Next"]').click();
     }
-    else{
-      await page.locator('[title="Previous"]')
+    else if(currentYear>targetYear){
+      await page.locator('[title="Prev"]').click();
+    } 
+
+    else if(currentYear==targetYear &&  currmonthNumber>targetMonthInNumber){
+
+      await page.locator('[title="Prev"]').click()
+      
+      
+      }
+
+      else if(currentYear==targetYear && currmonthNumber<targetMonthInNumber){
+
+        await page.locator('[title="Next"]').click()
+        
+        }    
+
+
     }
-  }
+    await page.waitForTimeout(5000)
+
+
 
 });
